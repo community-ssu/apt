653072fb7329f235a0e7aac591c85c865c2ae3b3
Mario Sanchez Prada <msanchez@igalia.com>
2009-12-02 16:06:49 +0100

    Revert "Set NoTriggers to true only in source, because its breaks scratchbox"
    This reverts commit 072479aaf38817acfb4a9f0eb00a0d320cceff53.

--- apt-pkg/deb/dpkgpm.cc
+++ apt-pkg/deb/dpkgpm.cc
@@ -601,7 +601,7 @@ bool pkgDPkgPM::Go(int OutStatusFd)
 
    unsigned int const MaxArgs = _config->FindI("Dpkg::MaxArgs",8*1024);
    unsigned int const MaxArgBytes = _config->FindI("Dpkg::MaxArgBytes",32*1024);
-   bool const NoTriggers = _config->FindB("DPkg::NoTriggers", true);
+   bool const NoTriggers = _config->FindB("DPkg::NoTriggers", false);
 
    if (RunScripts("DPkg::Pre-Invoke") == false)
       return false;
